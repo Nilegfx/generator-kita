4324c01ab09dcfa7b2d77fb9d45cd4074305052e
user <great.nile@gmail.com>
2019-06-23 16:20:42 +0200

    add optional starter packages

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,34 +3,62 @@ var { join } = require('path');
 const editJsonFile = require("edit-json-file");
 const glob = require('glob')
 
+const dependencyMapper = {
+  babel7: {
+    dev: true,
+    packages: ['@babel/core', '@babel/node', '@babel/preset-env']
+  },
+  jest: { dev: true, packages: ['jest'] }
+}
+
+const suggestedDependencies = Object.keys(dependencyMapper);
+const toPackages = (acc, dependency) => [...acc, ...dependencyMapper[dependency].packages];
+const isDevDependency = dependency => dependencyMapper[dependency].dev;
+const isProductionDependency = dependency => !dependencyMapper[dependency].dev;
+
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     this.argument('name', { required: false });
     this.option('babel'); // This method adds support for a `--babel` flag
-  }
-
-  initializing() {
-    this.projectName = this.options['name'];
+    this.answers = {}
   }
 
   async prompting() {
+    const done = this.async();
+    const questions = [{
+      type: "checkbox",
+      name: "dependencies",
+      message: "what else to include?",
+      choices: suggestedDependencies
+    }];
 
-    if (!this.projectName) {
-      const answers = await this.prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "Your project name"
-        }
-      ]);
-      this.projectName = answers.name;
+    if (!this.options['name']) {
+      questions.unshift({
+        type: "input",
+        name: "name",
+        message: "Your project name"
+      });
     }
+
+    try {
+      this.answers = await this.prompt(questions);
+      done();
+    } catch (e) {
+      this.error(e)
+    }
+
+  }
+
+  configuring() {
+    this.projectName = this.answers.name || this.options['name'];
   }
 
   writing() {
-    this.log('writing files .. ');
+    
+    this.log.invoke('writing files .. ');
     this._copyingTemplate();
+
     this.log(`adding project to ${this.destinationPath()} workspaces.. `);
     this._addProjectToDestinationWorkspaces();
 
@@ -58,13 +86,31 @@ module.exports = class extends Generator {
     }
   }
 
+  _installProjectDependencies() {
+    const devDependencies = this.answers.dependencies.filter(isDevDependency).reduce(toPackages, []);
+    const productionDependencies = this.answers.dependencies.filter(isProductionDependency).reduce(toPackages, []);
+
+    // install dev dependencies
+    this.yarnInstall(devDependencies, {
+      cwd: this.destinationPath(this.projectName),
+      dev: true
+    });
+
+    // install production dependencies
+    this.yarnInstall(productionDependencies, {
+      cwd: this.destinationPath(this.projectName)
+    });
+  }
+
   install() {
     this.log('installing dependencies...')
+    this._installProjectDependencies();
     this.yarnInstall(null, { cwd: this.destinationPath() });
   }
 
   end() {
-    this.spawnCommand('code', [this.destinationPath(this.projectName)]);
+    process.exit(0);
+    // this.spawnCommand('code', [this.destinationPath(this.projectName)]);
   }
 
 };
